ceab21e409439136061382efeb6b2cca5840ed67
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-15 13:28:12 +0530

    feat: enhance app structure with React Query integration and animation styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./app/App.css";
-import App from "./app/App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
+import { router } from "./app/routes/routes.jsx";
+import { RouterProvider } from "react-router-dom";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </Provider>,
 );
