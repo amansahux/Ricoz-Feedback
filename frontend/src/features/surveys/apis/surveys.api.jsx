a602326addb5f3302d48933d9662fb4106f3cc85
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-17 13:37:48 +0530

    feat: add survey API functions for CRUD operations

---
+++ frontend/src/features/surveys/apis/surveys.api.jsx
@@ -0,0 +1,62 @@
+import apiClient from "../../../../config/axiosInsstance";
+
+export const createSurvey = async (data) => {
+    try {
+        const response = await apiClient.post("/surveys", data);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating survey:", error);
+        throw error;
+    }
+}
+
+
+export const getAllSurveys = async () => {
+    try {
+        const response = await apiClient.get("/surveys");
+        return response.data;
+    } catch (error) {
+        console.error("Error getting surveys:", error);
+        throw error;
+    }
+}
+
+export const getSurveyById = async (surveyId) => {
+    try {
+        const response = await apiClient.get(`/surveys/${surveyId}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error getting survey:", error);
+        throw error;
+    }
+}
+
+export const updateSurvey = async (surveyId, surveyData) => {
+    try {
+        const response = await apiClient.put(`/surveys/${surveyId}`, surveyData);
+        return response.data;
+    } catch (error) {
+        console.error("Error updating survey:", error);
+        throw error;
+    }
+}
+
+export const deleteSurvey = async (surveyId) => {
+    try {
+        const response = await apiClient.delete(`/surveys/${surveyId}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error deleting survey:", error);
+        throw error;
+    }
+}
+
+export const publishSurvey = async (surveyId) => {
+    try {
+        const response = await apiClient.patch(`/surveys/${surveyId}/publish`);
+        return response.data;
+    } catch (error) {
+        console.error("Error publishing survey:", error);
+        throw error;
+    }
+}
