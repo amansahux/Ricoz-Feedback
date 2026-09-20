407311ebaa2863a1d129fdd083d07796717ed26f
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-20 12:15:18 +0530

    feat: enhance useSetting hook to include changePassword mutation

--- frontend/src/features/setting/hooks/useSetting.jsx
+++ frontend/src/features/setting/hooks/useSetting.jsx
@@ -1,5 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import { updateUser, changePassword } from "../apis/setting.api";
 
 export const useSetting = () => {
+  const updateUserMutation = useMutation({
+    mutationFn: updateUser,
+  });
 
-  return {};
+  const changePasswordMutation = useMutation({
+    mutationFn: changePassword,
+  });
+
+  return {
+    updateUserMutation,
+    changePasswordMutation,
+  };
 };
