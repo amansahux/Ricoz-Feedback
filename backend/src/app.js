4b7d0dfb7ac01f17c90a4ef475e8ae2c48d66756
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-25 13:00:30 +0530

    fix images confliction

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use(
       directives: {
         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
         'default-src': ["'self'"],
-        'img-src': ["'self'", 'data:', 'blob:', 'https://lh3.googleusercontent.com', 'https://*.googleusercontent.com', 'https://ik.imagekit.io', 'https://ik.imagekit.io/recoz'],
+        'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
         'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
         'font-src': ["'self'", 'data:', 'https://fonts.gstatic.com'],
         'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
