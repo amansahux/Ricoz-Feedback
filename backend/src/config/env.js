38f4ff30b258252933a128f9a9a4cac4bc496280
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-25 14:59:03 +0530

    Change the env variables for authenticaton

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,7 +5,8 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
+ ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+ REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
   CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
   NODE_ENV: process.env.NODE_ENV || 'development',
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
@@ -16,7 +17,7 @@ export const env = {
 };
 
 // Validate required env vars
-if (!env.MONGO_URI || !env.JWT_SECRET || !env.GOOGLE_CLIENT_ID || !env.GOOGLE_CLIENT_SECRET || !env.GOOGLE_REFRESH_TOKEN || !env.GOOGLE_EMAIL_ID) {
-  throw new Error('Missing required environment variables: MONGO_URI, JWT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN, GOOGLE_EMAIL_ID');
+if (!env.MONGO_URI || !env.ACCESS_TOKEN_SECRET || !env.REFRESH_TOKEN_SECRET || !env.GOOGLE_CLIENT_ID || !env.GOOGLE_CLIENT_SECRET || !env.GOOGLE_REFRESH_TOKEN || !env.GOOGLE_EMAIL_ID) {
+  throw new Error('Missing required environment variables: MONGO_URI, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN, GOOGLE_EMAIL_ID');
 
 }
